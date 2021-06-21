23158eabc245b0471eaa077e45824edcc83a3f19
Devulak <3112566+ZeroXitreo@users.noreply.github.com>
2021-06-21 12:57:44 +0200

    Create galactic_fail.lua

---
+++ lua/autorun/galactic_fail.lua
@@ -0,0 +1,10 @@
+hook.Add("PostGamemodeLoaded", "GalacticFail", function(ply)
+	if not galactic && not galacticFail then
+		galacticFail = true
+		if SERVER then
+			MsgC(Color(255, 55, 55), "Galactic Core not installed, please install it from the workshop!\n")
+		else
+			chat.AddText(Color(255, 55, 55), "Galactic Core not installed, please install it from the workshop!")
+		end
+	end
+end)
